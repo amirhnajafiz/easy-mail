77f6b012238ba7bdb106aa8f0487bb24631da068
amirhnajafiz <najafizadeh21@gmail.com>
2022-03-30 14:02:53 +0430

    mail main function init

--- internal/mail/mail.go
+++ internal/mail/mail.go
@@ -1,1 +1,15 @@
 package mail
+
+import "github.com/mailgun/mailgun-go"
+
+func SendSimpleMessage(domain, apiKey string) (string, error) {
+	mg := mailgun.NewMailgun(domain, apiKey)
+	m := mg.NewMessage(
+		"Excited User <elliot@tutorialedge.net>",
+		"Hello",
+		"Testing some Mailgun!",
+		"elliot@tutorialedge.net",
+	)
+	_, id, err := mg.Send(m)
+	return id, err
+}
