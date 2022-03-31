7737235709c3e431ef6f540badf65ec2442d0384
amirhnajafiz <najafizadeh21@gmail.com>
2022-03-31 09:22:50 +0430

    mail init func created

--- internal/mail/mail.go
+++ internal/mail/mail.go
@@ -3,18 +3,22 @@ package mail
 import "github.com/mailgun/mailgun-go"
 
 type Mail struct {
-	Cfg Config
+	Cfg    Config
+	Client *mailgun.MailgunImpl
 }
 
-func (m Mail) SendSimpleMessage() (string, error) {
-	mg := mailgun.NewMailgun(m.Cfg.Domain, m.Cfg.APIKEY)
-	ms := mg.NewMessage(
+func (m *Mail) Init() {
+	m.Client = mailgun.NewMailgun(m.Cfg.Domain, m.Cfg.APIKEY)
+}
+
+func (m *Mail) SendSimpleMessage() (string, error) {
+	ms := m.Client.NewMessage(
 		"Excited User <elliot@tutorialedge.net>",
 		"Hello",
 		"Testing some Mailgun!",
 		"elliot@tutorialedge.net",
 	)
-	_, id, err := mg.Send(ms)
+	_, id, err := m.Client.Send(ms)
 
 	return id, err
 }
