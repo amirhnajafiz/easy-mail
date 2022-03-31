9b8c11fbc782dc08cf47787e735679bcc2dc70f0
amirhnajafiz <najafizadeh21@gmail.com>
2022-03-31 21:38:06 +0430

    send mail handler added

--- internal/http/handler/handler.go
+++ internal/http/handler/handler.go
@@ -1,7 +1,33 @@
 package handler
 
-import "github.com/amirhnajafiz/easy-mail/internal/mail"
+import (
+	"net/http"
+
+	"github.com/amirhnajafiz/easy-mail/internal/mail"
+	"github.com/gin-gonic/gin"
+)
 
 type Handler struct {
 	Postman mail.Mail
+}
+
+func (h Handler) SendMail(c *gin.Context) {
+	var (
+		validation = true
+		isHTML     = false
+	)
+
+	env := mail.Envelope{
+		From:    c.Param("from"),
+		Subject: c.Param("subject"),
+		Text:    c.Param("text"),
+		To:      c.Param("to"),
+	}
+
+	id, err := h.Postman.Send(env, validation, isHTML)
+	if err != nil {
+		_ = c.Error(err)
+	}
+
+	c.String(http.StatusOK, id)
 }
