3a4cdbe9140c4eeaac619176f2341910b83eaa9b
amirhnajafiz <najafizadeh21@gmail.com>
2022-03-31 09:15:36 +0430

    config initalized

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,11 @@
 package config
+
+import "github.com/amirhnajafiz/easy-mail/internal/mail"
+
+type Config struct {
+	MailGun mail.Config `koanf:"mail_gun"`
+}
+
+func Load() Config {
+	return Default()
+}
--- internal/config/default.go
+++ internal/config/default.go
@@ -1,1 +1,12 @@
 package config
+
+import "github.com/amirhnajafiz/easy-mail/internal/mail"
+
+func Default() Config {
+	return Config{
+		MailGun: mail.Config{
+			Domain: "",
+			APIKEY: "",
+		},
+	}
+}
