cf0b19c7b76a10f9c376b9793fec67805539c050
amirhnajafiz <najafizadeh21@gmail.com>
2022-04-04 14:01:08 +0430

    refactor k6 testing

--- k6/script.js
+++ k6/script.js
@@ -2,6 +2,21 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 
 export default function () {
-    http.get('https://test.k6.io');
+    const url = 'http://127.0.0.1:5000/mail/send';
+    const payload = JSON.stringify({
+        from: "najafizadeh21@gmail.com",
+        to: "najafizadeh21@gmail.com",
+        subject: "Easy mail",
+        text: "This is a test message.",
+        validation: "no"
+    });
+
+    const params = {
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    };
+
+    http.post(url, payload, params);
     sleep(1);
 }
